20f71eb6678fe17b90cd04be95da7e1cfd859eb5
illusions333 <1alexillusions1@gmail.com>
2026-03-21 19:25:32 +0100

    Add Rental and RentalRepository classes

---
+++ tutorial03/Rental.cs
@@ -0,0 +1,33 @@
+﻿namespace tutorial03;
+
+public class Rental
+{
+    public long UserId { get; private set; }
+    public long EquipmentId { get; private set; }
+    public DateTime RentalDate { get; private set; }
+    public DateTime DueDate { get; private set; }
+    public DateTime? ReturnDate { get; private set; }
+    public double RentalFee { get; private set; }
+
+    public Rental(long userId, long equipmentId, DateTime rentalDate, DateTime dueDate)
+    {
+        UserId = userId;
+        EquipmentId = equipmentId;
+        RentalDate = rentalDate;
+        if (DueDate < RentalDate)
+            throw new ArgumentException("Due date cannot be before rental date.");
+        DueDate = dueDate;
+        ReturnDate = null;
+        RentalFee = 0.0;
+    }
+    
+    public void Return(DateTime returnDate, double rentalFee)
+    {
+        if (ReturnDate != null)
+            throw new InvalidOperationException("Equipment has already been returned.");
+        if (returnDate < RentalDate)
+            throw new ArgumentException("Return date cannot be before rental date.");
+        ReturnDate = returnDate;
+        RentalFee = rentalFee;
+    }
+}
---
+++ tutorial03/RentalRepository.cs
@@ -0,0 +1,49 @@
+﻿namespace tutorial03;
+
+public class RentalRepository
+{
+    private List<Rental> _rentals;
+    
+    public RentalRepository()
+    {
+        _rentals = new List<Rental>();
+    }
+    
+    public void AddRental(Rental rental)
+    {
+        _rentals.Add(rental);
+    }
+
+    public Rental GetRental(long rentalId)
+    {
+        foreach (var rental in _rentals)
+        {
+            if (rental.UserId == rentalId) return rental;
+        }
+        throw new KeyNotFoundException("Rental with ID " + rentalId + " not found.");
+    }
+    public List<Rental> GetAllRentals()
+    {
+        return _rentals;
+    }
+    public void RemoveRental(long rentalId)
+    {
+        Rental? rentalToRemove = null;
+        foreach (var rental in _rentals)
+        {
+            if (rental.UserId == rentalId)
+            {
+                rentalToRemove = rental;
+                break;
+            }
+        }
+        if (rentalToRemove != null)
+        {
+            _rentals.Remove(rentalToRemove);
+        }
+        else
+        {
+            throw new KeyNotFoundException("Rental with ID " + rentalId + " not found.");
+        }
+    }
+}
